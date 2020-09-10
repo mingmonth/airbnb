46994bdbcd46688db36bb1c2009d83b42c515fbb
김윤수 <yskim@taeha.co.kr>
2020-09-10 11:43:23 +0900

    paginator2

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -46,7 +46,7 @@ class Command(BaseCommand):
         print(created_clean)
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
-            for i in range(3, random.randint(10, 17)):
+            for i in range(3, random.randint(10, 30)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
--- rooms/views.py
+++ rooms/views.py
@@ -7,7 +7,7 @@ from . import models
 def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 5)
+    paginator = Paginator(room_list, 5, orphans=1)
     rooms = paginator.get_page(page)
     print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    return render(request, "rooms/home.html", {"page": rooms})
