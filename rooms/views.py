c2afe8946dbd45f8e52465d7a88dfdfa5862e91b
김윤수 <yskim@taeha.co.kr>
2020-09-11 12:48:47 +0900

    CBV first

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,20 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 5, orphans=1)
+class HomeView(ListView):
 
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        rooms = paginator.page(1)
-        return redirect("/")
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 5
+    paginate_orphans = 1
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
